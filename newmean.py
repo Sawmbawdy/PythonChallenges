efef5ebed06d635749bcf777ee5d0ca1519c77e0
Sawmbawdy <saiompatro12@gmail.com>
2025-07-25 13:45:20 -0400

    Add files via upload

---
+++ newmean.py
@@ -0,0 +1,11 @@
+Totalno = 40
+wrongno = 36
+actual = 56
+mean1 = 38
+sum = Totalno*mean1
+print(sum)
+num2 = sum - ((wrongno) - (actual))
+print(num2)
+mean2 = num2 / Totalno
+print("The new mean is", mean2)
+
