031768a8b2adafc6479127125aed82241d43b049
Sawmbawdy <saiompatro12@gmail.com>
2025-07-25 13:16:08 -0400

    Add files via upload

---
+++ ComAndArth.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+c = 3
+d = 4
+e = ((a+b)**d-c)**(a+b)**d-c
+print(e)
+
+name = str(input("Enter your name: "))
+Age = int(input("Enter your age: "))
+if name == "Saiom" and Age <= 12:
+    print("Hello Saiom, welcome to the program!")
+else:
+    print("Bye", name, ", see you next time!")
