4ba2baa5a67749000cad51e0cf40f8710801376a
Sawmbawdy <saiompatro12@gmail.com>
2025-08-01 11:22:28 -0400

    Add files via upload

---
+++ AvgSpeed.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the speed (mph): \n"))
+b = int(input("Enter the speed (mph): \n"))
+c = int(input("Enter the speed (mph): \n"))
+
+avg = a + b + c
+avg /= 3
+
+print("The average speed is", avg, "mph.")
